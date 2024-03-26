f2f59d6d36902c709c0438eccb36dc38e368bf3b
brianchege2k <chegebrian84@gmail.com>
2024-03-26 15:08:35 +0300

    arrow functions lab

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide(num1,num2){
+
+    return num1/num2
+}
+
+console.log(divide (2000,100))
+
+
+const square = (num) =>{
+    return num * num;
+}
+
+console.log(square(20))
+
+
+const add = (num1,num2) =>{
+    return num1 + num2;
+}
+
+console.log(add(20,20))
